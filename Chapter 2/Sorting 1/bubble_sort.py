a2ff3e259469db64ba8556199ec8740b378f1f34
shubanms <msshuban4@gmail.com>
2024-03-04 22:55:08 +0530

    Added sorting algorithms

---
+++ Chapter 2/Sorting 1/bubble_sort.py
@@ -0,0 +1,25 @@
+def bubble_sort(
+    array: list
+) -> list:
+    
+    '''
+    This function sorts a list using the bubble sort
+    algorithm and returns an array.
+    This algorithm has a time complexity of O(n) and space complexity
+    of O(n).
+    '''
+    
+    for i in range(len(array)-1):
+        for j in range(i, len(array)):
+            if(array[j] < array[i]):
+                array[i], array[j] = array[j], array[i]
+                
+    return array
+    
+    
+if __name__ == "__main__":
+    array = list(map(int, input("Enter the elements: ").split()))
+    print(f"Un-sorted array is {array}")
+    
+    sorted_array = bubble_sort(array)
+    print(f"Sorted array is {sorted_array}")
---
+++ Chapter 2/Sorting 1/selection_sort.py
@@ -0,0 +1,26 @@
+def selection_sort(
+    array: list
+) -> list:
+    
+    '''
+    This function sorts a given array and return the sorted array.
+    It uses Selection sort of time complexity O(n) and space complexity is O(1),
+    as it is an inplace sort algorithm.
+    '''
+    
+    for i in range(len(array)-1):
+        mini = i
+        for j in range(i, len(array)):
+            if(array[j] < array[mini]):
+                mini = j
+        array[i], array[mini] = array[mini], array[i]
+        
+    return array
+    
+    
+if __name__ == "__main__":
+    array = list(map(int, input("Enter the elements: ").split()))
+    print(f"Un-sorted array is {array}")
+    
+    sorted_array = selection_sort(array)
+    print(f"Sorted array is {sorted_array}")
