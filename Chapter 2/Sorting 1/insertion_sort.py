c86b0a3e77ccfbd85b0990fa9cc21c0a522afa3e
shubanms <msshuban4@gmail.com>
2024-03-08 22:41:08 +0530

    Formatting changes

--- Chapter 2/Sorting 1/insertion_sort.py
+++ Chapter 2/Sorting 1/insertion_sort.py
@@ -1,26 +1,25 @@
 def insertion_sort(
     array: list
 ) -> list:
-    
     '''
     This function takes an array and sorts it using
     the insertion sort algorithm and returns the sorted array.
     This is in time complexity of O(n) and space complexity O(1),
     as it is a inplace sorting algorithm
     '''
-    
+
     for i in range(len(array)):
         j = i
-        while(j > 0 and array[j-1]>array[j]):
+        while (j > 0 and array[j-1] > array[j]):
             array[j-1], array[j] = array[j], array[j-1]
             j -= 1
-            
+
     return array
-    
+
 
 if __name__ == "__main__":
     array = list(map(int, input("Enter the elements: ").split()))
     print(f"Un-sorted array is {array}")
-    
+
     sorted_array = insertion_sort(array)
     print(f"Sorted array is {sorted_array}")
--- Chapter 2/Sorting 2/recursive_bubble_sort.py
+++ Chapter 2/Sorting 2/recursive_bubble_sort.py
@@ -2,26 +2,25 @@ def bubble_sort(
     array: list,
     n: int
 ) -> None:
-    
     '''
     This function sorts an array and updates the values in place.
     This uses an recursive method of bubble sort.
     The time complexity is O(N^2) and space is O(N).
     '''
 
-    if(n == 1):
+    if (n == 1):
         return
-    
+
     for i in range(n - 1):
-        if(array[i] > array[i+1]):
+        if (array[i] > array[i+1]):
             array[i], array[i+1] = array[i+1], array[i]
-        
-    bubble_sort(array, n-1)    
-    
+
+    bubble_sort(array, n-1)
+
 
 if __name__ == "__main__":
     array = list(map(int, input("Enter the elements: ").split()))
     print(f"Un-sorted array is {array}")
-    
+
     bubble_sort(array, len(array))
     print(f"Sorted array is {array}")
